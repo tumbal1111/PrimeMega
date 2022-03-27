317e82795e6d105755486c1d3538ac272b59084d
tumbal1111 <102343757+tumbal1111@users.noreply.github.com>
2022-03-27 10:09:40 +0800

    Update __init__.py

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -220,10 +220,9 @@ else:
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
-DRAGONS.add(2137482758)
-DRAGONS.add(1732814103)
+DRAGONS.add(2064267938)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1416529201)
+DEV_USERS.add(2048936969)
 
 
 if not SPAMWATCH_API:
